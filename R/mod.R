06f62d14e342873fb7f307039e15f92b6001bfb5
kuraisle <jamesjimitchell@gmail.com>
2016-07-25 13:33:20 +0100

    Update mod.R
    corrected typo line 7, now refers to existing package

--- R/mod.R
+++ R/mod.R
@@ -4,6 +4,6 @@
 #' @export
 
 mod <- function(modelFormula, timepoint, startPars){
-  myMod <- minpack.LM::nlsLM(formula = modelFormula, data = timepoint, start = startPars, lower = c(0,0,0))
+  myMod <- minpack.lm::nlsLM(formula = modelFormula, data = timepoint, start = startPars, lower = c(0,0,0))
   myMod
 }
