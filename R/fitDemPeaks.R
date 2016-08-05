44401085203cf42316eed76f4aec0ea924077862
kuraisle <jamesjimitchell@gmail.com>
2016-08-05 14:11:35 +0100

    Added option for intensities <0
    the belowZero argument is default FALSE, meaning models cannot return intensities below zero. If this is changed to true, there is no lower limit on intensities

--- R/fitDemPeaks.R
+++ R/fitDemPeaks.R
@@ -5,15 +5,22 @@
 #' @param guesses A list of starting guesses for your data. nls.LM is smart enough that these usually just need to be of the right order of magnitude. In a list, called "i1", "i2" etc.
 #' @param wavelengths A vector of the wavelengths your data is over.
 #' @param spectra A list of data frames containing wavelength and absorbance data.
+#' @param belowZero An optional logical value. Defaults to false, meaning intensities bottom out at zero.
 #' @return A matrix of intensities for each peak in fixed.
 #' @export
 
-fitDemPeaks <- function(fixed, guesses, spectra, wavelengths){
+fitDemPeaks <- function(fixed, guesses, spectra, wavelengths, belowZero = FALSE){
   #generate a set of peaks with the specified centres and widths
   myModelFormula <- gaussgen(fixedParameters = fixed, wavelength = wavelengths)
   #myModel uses mod to build an nlsLM function which only takes the timepoint as an argument so laply can use it
+  goBelow <- c()
+  if(belowZero == TRUE){
+    goBelow <- NULL
+  } else {
+    goBelow <- numeric(ncol(fixed))
+  }
   myModel <- function(specTimePoint){
-    theModel <- mod(modelFormula = myModelFormula, timepoint = specTimePoint, startPars = guesses)
+    theModel <- mod(modelFormula = myModelFormula, timepoint = specTimePoint, startPars = guesses, bZ = goBelow)
     coef(theModel)
   }
   #This applies myModel to each timepoint in a list and returns a matrix containing intensities
