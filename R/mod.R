16255167a6195de7421e39318b09e8fb2ad37c8c
kuraisle <jamesjimitchell@gmail.com>
2016-08-05 14:06:27 +0100

    Adding below Zero option back in
    setting bZ to NULL will allow default behaviour

--- R/mod.R
+++ R/mod.R
@@ -4,10 +4,11 @@
 #' @param modelFormula a formula the nlsLM fits
 #' @param timepoint the spectrum of a timepoint
 #' @param startPars the starting guesses for peak intensities
+#' @param bZ An external control allowing a lower limit to intensity. Allows fitDemPeaks to have a control for whether or not to go below zero.
 #' @export
 #' @keywords internal
 
-mod <- function(modelFormula, timepoint, startPars){
-  myMod <- minpack.lm::nlsLM(formula = modelFormula, data = timepoint, start = startPars)
+mod <- function(modelFormula, timepoint, startPars, bZ){
+  myMod <- minpack.lm::nlsLM(formula = modelFormula, data = timepoint, start = startPars, lower = bZ)
   myMod
 }
